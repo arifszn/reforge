4f75264aa8c7d2a08b4247161b3588d06760afa9
Ariful Alam <ariful.alam@monstar-lab.com>
2023-03-24 16:07:16 +0600

    Delete a user

--- src/components/users/index.tsx
+++ src/components/users/index.tsx
@@ -1,15 +1,45 @@
-import { ProTable, ProColumns, RequestData } from '@ant-design/pro-components';
-import { Avatar, BreadcrumbProps } from 'antd';
-import { useRef } from 'react';
+import {
+  ActionType,
+  ProTable,
+  ProColumns,
+  RequestData,
+  TableDropdown,
+} from '@ant-design/pro-components';
+import {
+  Avatar,
+  BreadcrumbProps,
+  Dropdown,
+  Menu,
+  MenuProps,
+  Modal,
+  Popconfirm,
+  Space,
+} from 'antd';
+import { createContext, useRef, useState } from 'react';
 import { FiUsers } from 'react-icons/fi';
+import { CiCircleMore } from 'react-icons/ci';
 import { Link } from 'react-router-dom';
 import { User } from '../../interfaces/models/user';
 import { apiRoutes } from '../../routes/api';
 import { webRoutes } from '../../routes/web';
-import { handleErrorResponse } from '../../utils';
+import {
+  handleErrorResponse,
+  NotificationType,
+  showNotification,
+} from '../../utils';
 import http from '../../utils/http';
 import BasePageContainer from '../layout/PageContainer';
 import LazyImage from '../lazy-image';
+import Icon, {
+  DownOutlined,
+  ExclamationCircleOutlined,
+  EditOutlined,
+  DeleteOutlined,
+} from '@ant-design/icons';
+
+enum ActionKey {
+  DELETE = 'delete',
+}
 
 const breadcrumb: BreadcrumbProps = {
   items: [
@@ -24,49 +54,113 @@ const breadcrumb: BreadcrumbProps = {
   ],
 };
 
-const columns: ProColumns[] = [
-  {
-    title: 'Avatar',
-    dataIndex: 'avatar',
-    align: 'center',
-    sorter: false,
-    render: (_, row) =>
-      row.avatar ? (
-        <Avatar
-          shape="circle"
-          size="small"
-          src={
-            <LazyImage
-              src={row.avatar}
-              placeholder={<div className="bg-gray-100 h-full w-full" />}
-            />
-          }
-        />
-      ) : (
-        <Avatar shape="circle" size="small">
-          {row.first_name.charAt(0).toUpperCase()}
-        </Avatar>
-      ),
-  },
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    sorter: false,
-    align: 'center',
-    ellipsis: true,
-    render: (_, row) => `${row.first_name} ${row.last_name}`,
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
-    sorter: false,
-    align: 'center',
-    ellipsis: true,
-  },
-];
-
 const Users = () => {
-  const actionRef = useRef();
+  const actionRef = useRef<ActionType>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [modal, modalContextHolder] = Modal.useModal();
+
+  const columns: ProColumns[] = [
+    {
+      title: 'Avatar',
+      dataIndex: 'avatar',
+      align: 'center',
+      sorter: false,
+      render: (_, row) =>
+        row.avatar ? (
+          <Avatar
+            shape="circle"
+            size="small"
+            src={
+              <LazyImage
+                src={row.avatar}
+                placeholder={<div className="bg-gray-100 h-full w-full" />}
+              />
+            }
+          />
+        ) : (
+          <Avatar shape="circle" size="small">
+            {row.first_name.charAt(0).toUpperCase()}
+          </Avatar>
+        ),
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      sorter: false,
+      align: 'center',
+      ellipsis: true,
+      render: (_, row) => `${row.first_name} ${row.last_name}`,
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      sorter: false,
+      align: 'center',
+      ellipsis: true,
+    },
+    {
+      title: 'Action',
+      align: 'center',
+      key: 'option',
+      fixed: 'right',
+      render: (_, row) => [
+        <TableDropdown
+          key="actionGroup"
+          onSelect={(key) => handleActionOnSelect(key, row.id)}
+          menus={[
+            {
+              key: ActionKey.DELETE,
+              name: (
+                <Space>
+                  <DeleteOutlined />
+                  Delete
+                </Space>
+              ),
+            },
+          ]}
+        >
+          <Icon component={CiCircleMore} className="text-primary text-xl" />
+        </TableDropdown>,
+      ],
+    },
+  ];
+
+  const handleActionOnSelect = (key: string, id: number) => {
+    if (key === ActionKey.DELETE) {
+      showDeleteConfirmation(id);
+    }
+  };
+
+  const showDeleteConfirmation = (id: number) => {
+    modal.confirm({
+      title: 'Are you sure to delete the user?',
+      icon: <ExclamationCircleOutlined />,
+      okButtonProps: {
+        className: 'bg-primary',
+      },
+      onOk: () => {
+        setLoading(true);
+
+        http
+          .delete(`${apiRoutes.users}/${id}`)
+          .then(() => {
+            showNotification(
+              'Success',
+              NotificationType.SUCCESS,
+              'User is deleted.'
+            );
+
+            actionRef.current?.reloadAndRest?.();
+          })
+          .catch((error) => {
+            handleErrorResponse(error);
+          })
+          .finally(() => {
+            setLoading(false);
+          });
+      },
+    });
+  };
 
   return (
     <BasePageContainer breadcrumb={breadcrumb}>
@@ -119,6 +213,7 @@ const Users = () => {
           search: false,
         }}
       />
+      {modalContextHolder}
     </BasePageContainer>
   );
 };
